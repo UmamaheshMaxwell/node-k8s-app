c4584515410b038355ebbe7fc4031dc16bbec39d
UmamaheshMaxwell <maheshmeka16@gmail.com>
2023-03-23 16:29:04 +0530

    node k8s app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("<h1 style='color:blue'>Welcome to the world of kubernetes !!!</h1>")
+    res.send("<h1 style='color:blue;background-color:yellow;'>Welcome to the world of kubernetes !!!</h1>")
 })
 
 const PORT = 3000
