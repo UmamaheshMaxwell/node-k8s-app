bb18f1885d3d3dd5155f93be35467e427ef05826
UmamaheswararaoMeka <maheshmeka16@gmail.com>
2023-05-01 18:06:36 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from "express";
+import mongoose from "mongoose"
 
 const app = express()
 
+const MONGO_URL = process.env.MONGO_URL
+mongoose.connect(MONGO_URL)
+        .then("Connected to MongoDB Sucessfully")
+        .catch(err => console.log(err))
+
 app.get("/", (req, res) => {
-    res.send("<h1 style='color:blue;background-color:yellow;'>Welcome to the world of kubernetes !!!</h1>")
+    res.send("<h1 style='color:blue;background-color:coral;'>Welcome to the world of kubernetes !!!</h1>")
 })
 
-const port = process.env.PORT 
+const port = process.env.PORT | 3000
 
 app.listen(port, () => {
     console.log(`
-      🚀  Server is running!!!
+      🚀  Server is running!
       🔉  Listening on port ${port}
       📭  Click here http://localhost:${port}
     `);
